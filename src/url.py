252e52ac630241c5964d23debbcff0869d702af2
Jack <obri.jack.02@gmail.com>
2024-10-18 15:40:33 +1000

    Did half of first chapter on URL parsing and HTTP requests.

---
+++ src/url.py
@@ -0,0 +1,67 @@
+"""URL Parsing."""
+
+import socket
+
+
+class URL:
+    """URL object handling parsing and HTTP requests."""
+
+    def __init__(self, url: str):
+        """Initialise a URL by parsing the string into its components."""
+        self.scheme, url = url.split("://", maxsplit=1)
+        assert self.scheme == "http"
+
+        if "/" not in url:
+            self.host = url
+        else:
+            self.host, url = url.split("/", maxsplit=1)
+
+        self.path = "/" + url
+
+    def request(self):
+        """Download the webpage specified by self.
+
+        Returns: str
+            web content
+
+        """
+        # Create socket connection to self.host
+        host_connection_socket = socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
+            proto=socket.IPPROTO_TCP,
+        )
+        host_connection_socket.connect((self.host, 80))
+
+        # Create HTTP request
+        request = f"GET {self.path} HTTP/1.0\nHost {self.host}\n\n"
+        host_connection_socket.send(request.encode("utf8"))
+
+        # Get response as a string
+        # afaik makefile is just a helper which waits for all bits to arrive from the
+        # socket.
+        response = host_connection_socket.makefile("r", encoding="utf8")
+
+        # Parse the status line in the response
+        statusline = response.readline()
+        version, status, explanation = statusline.split(" ", maxsplit=2)
+
+        # Parse the headers in the response
+        response_headers = {}
+        while True:
+            line = response.readline()
+            if line == "\n":
+                break
+            header, value = line.split(":", maxsplit=1)
+            response_headers[header.lower()] = value.strip()
+
+        # Ensure that some weird encodings are not present? IDK
+        assert (
+            "transfer-encoding" not in response_headers
+            and "content-encoding" not in response_headers
+        )
+
+        # Get the web content and close the socket
+        content = response.read()
+        host_connection_socket.close()
+        return content
